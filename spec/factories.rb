723247a353a0dc34202f95d9191a9c18551d7ed8
Michelle Pan <m.pan@modcloth.com>
2013-06-20 16:37:13 -0700

    Finish user edit, update, index, destory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do 
 	factory :user do
-		name "Michelle Pan"
-		email "m.pan@modcloth.com"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "Person_#{n}@example.com" }
 		password "password"
 		password_confirmation "password"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
